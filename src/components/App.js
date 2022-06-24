6f5308e3303e6695433350eb5eee53071aadae5c
Nathan <hello@nathandorney.co.uk>
2022-06-24 15:27:58 +0100

    hiding css

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,6 @@ import Block16 from "./Block16";
 import Block17 from "./Block17";
 import Block18 from "./Block18";
 
-// styles
-import "../app.css";
-
 const date = new Date().toDateString();
 
 const App = () => {
